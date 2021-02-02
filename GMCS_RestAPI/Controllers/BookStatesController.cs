4cdf82ff4767459092d6251d810cb2d79d9e4072
Борис <krasilnikov.b@imatic.pro>
2021-02-02 12:17:02 +0300

    Класс для отслеживания доступных состояний книги

---
+++ GMCS_RestAPI/Controllers/BookStatesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GMCS_RestAPI.Database;
+using GMCS_RestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GMCS_RestAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class BookStatesController : Controller
+	{
+		private readonly ApplicationContext _context;
+
+		public BookStatesController(ApplicationContext context)
+		{
+			_context = context;
+		}
+
+		/// <summary>
+		///  Получение списка состояний книги.
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<BookState>>> Get()
+		{
+			return await _context.BookStates.ToListAsync();
+		}
+
+	}
+}
