aa399fbbf3e565d09493cf6440d65fac6633008c
bkrasilnikov <bkrasilnikov@gmcs.ru>
2021-02-17 14:14:33 +0300

    Логирование времени и вывода в консоль

---
+++ GMCS_RestAPI/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMCS_RestAPI.Middlewares
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger logger;
+
+        private Stopwatch _timer;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            this.next = next;
+            logger = loggerFactory.CreateLogger<RequestResponseLoggingMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _timer = Stopwatch.StartNew();
+            context.Request.EnableBuffering();
+
+            var buffer = new byte[Convert.ToInt32(context.Request.ContentLength)];
+            await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+            var requestBody = Encoding.UTF8.GetString(buffer);
+            context.Request.Body.Seek(0, SeekOrigin.Begin);
+
+            logger.LogInformation(requestBody);
+
+            var originalBodyStream = context.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                context.Response.Body = responseBody;
+
+                await next(context);
+
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+                var response = await new StreamReader(context.Response.Body).ReadToEndAsync();
+                context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+                _timer.Stop();
+
+                logger.LogInformation($"Time: {_timer.ElapsedMilliseconds} \r\n Body:{response}");
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+    }
+}
